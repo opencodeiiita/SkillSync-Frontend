7a1ccc529676b2776783497297de6ee7027d3c25
Atharwa23 <iit2023061@iiita.ac.in>
2024-12-19 10:07:09 +0530

    Added a Theme Toggle Button and a Logo Redirect to the Navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 
-const Home = () => {
+const Home = ({isDarkMode,setIsDarkMode}) => {
+  
   return (
     <div className="flex flex-col h-screen">
-      <Navbar /> {/* Navbar */}
-      <div className="flex-grow flex items-center justify-center">
+      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/> {/* Navbar */}
+      <div className={`flex-grow flex items-center justify-center ${isDarkMode ? 'bg-gray-800 text-white':'bg-white text-black'}`}>
         <h1 className="text-4xl font-bold">Hello</h1>
       </div>
     </div>
